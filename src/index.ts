0e3166fe2979dc5df9eda1ae70e420d74912bb39
Kaushik R Bangera <50287102+NeuronEnix@users.noreply.github.com>
2025-12-01 10:01:01 +0000

    refactor: remove USER_ROLE export from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { TDefaultCtx, USER_ROLE } from "./ctx/ctx.types";
+import { TDefaultCtx } from "./ctx/ctx.types";
 import { transformFromExpress } from "./transform/fromExpress";
 import { CtxRouter } from "./ctx/ctx.router";
 import { CtxError, ctxErrMap } from "./ctx/ctx.err";
 export type { TDefaultCtx };
-export { USER_ROLE, CtxRouter, CtxError, ctxErrMap };
+export { CtxRouter, CtxError, ctxErrMap };
 
 export namespace toCtx {
   export const fromExpress = transformFromExpress;
